ede38cba78ef361e3e3e192175bbd910e3efa56a
Rick <rick8220602@gmail.com>
2025-11-26 13:38:16 +0800

    test

--- backend/src/main/java/com/rick/backend/facade/HelloFacade.java
+++ backend/src/main/java/com/rick/backend/facade/HelloFacade.java
@@ -1,10 +1,8 @@
 package com.rick.backend.facade;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
 public interface HelloFacade {
 
     String hello();
 
-    String echo(@RequestBody String message);
+    String echo(String message);
 }
